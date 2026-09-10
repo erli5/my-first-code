18cbd7802f37aa3020c5ca4bba6204e9bb871359
erli5 <2488179027@qq.com>
2026-09-10 17:29:14 +0800

    Enhance guess number game with best score tracking
    Add functionality to track and save the best score.

--- guess_number.py
+++ guess_number.py
@@ -1,19 +1,19 @@
 import random
-#生成1-100之间随机整数
+# 生成1-100之间随机整数
 answer = random.randint(1,100)
 print("====猜数字游戏====")
 print("我想好了一个1到100之间的数字,来猜猜看吧!")
-count = 0 #记录猜的次数
-best =None try:
-    with open("best_score.txt","r")as f:
-        best = int(f.read())
-        except:
-            pass # 第一次运行，文件还不存在，跳过
+count = 0 # 记录猜的次数
+best =None
+try:
+    with open("best_score.txt","r") as f:best = int(f.read())
+except:
+    pass # 第一次运行，文件不存在，跳过
 while True:
-    #获取用户输入
+    # 获取用户输入
     guess_str = input("请输入你猜的数字:")
 
-    #判断不是数字
+    # 判断不是数字
     if not guess_str.isdigit():
         print("请输入有效的整数!")
         continue
@@ -27,4 +27,13 @@ while True:
     else:
         print(f"恭喜你猜对啦!答案就是{answer}")
         print(f"你一共猜了{count}次")
+        if best is None or count < best:
+            print("新纪录！")
+            with open("best_score.txt","w") as f:
+                f.write(str(count))
+        else:
+            print(f"历史最好的成绩是{best}次，加油！")
+
         break
+
+
